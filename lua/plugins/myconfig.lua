aca9ab4b8d8a580a1d14543855131cfa87a759de
Lyon <534791@student.saxion.nl>
2025-10-15 11:52:07 +0200

    Initial config

---
+++ lua/plugins/myconfig.lua
@@ -0,0 +1,55 @@
+require("catppuccin").setup({ auto_integrations = true })
+
+return {
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        require("lazyvim").setup({ colorscheme = "catppuccin-mocha" })
+        --        vim.api.nvim_set_option_value("colorscheme", "catppuccin-mocha", {})
+      end,
+
+      set_light_mode = function()
+        require("lazyvim").setup({ colorscheme = "catppuccin-latte" })
+
+        --        vim.api.nvim_set_option_value("colorscheme", "catppuccin-latte", {})
+      end,
+
+      update_interval = 100,
+      fallback = "dark",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "cpp" },
+      folds = { enable = true },
+      hightlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = (function()
+        if vim.o.background == "dark" then
+          return "catppuccin-mocha"
+        else
+          return "catppuccin-latte"
+        end
+      end)(),
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        layout = {
+          layout = {
+            position = "right",
+          },
+        },
+      },
+    },
+  },
+}
